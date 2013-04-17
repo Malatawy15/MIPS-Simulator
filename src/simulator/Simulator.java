ff5aecb8b6c6c3b7b03299c0079ceb3e1b3e5f7c
Maged Shalaby <maged918@gmail.com>
2013-04-17 22:58:21 +0200

    Added empty load_data method

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -4,6 +4,11 @@ public class Simulator {
 	
 	public static void main (String[]args){
 		System.out.println("Hello World! --Git Style ;)");
+		load_data();
+	}
+	
+	public static void load_data(){
+		
 	}
 
 }
