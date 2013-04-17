89ac7c0319dec8055e2016c62b59c160c300ad95
malatawy15 <malatawy15@gmail.com>
2013-04-17 22:35:07 +0200

    Dayna's first commit

--- src/components/Multiplexer.java
+++ src/components/Multiplexer.java
@@ -1,5 +1,11 @@
 package components;
 
 public class Multiplexer {
-
+	public static int value1;
+	public static int value2;
+	public static int complementOfValue1;
+	public Multiplexer ()
+	{
+		
+	}
 }
