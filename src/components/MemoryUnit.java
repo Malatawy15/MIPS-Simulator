2ddb2227c4ba6ecf68b56bae7635ee8aff7bd41c
Maged Shalaby <maged918@gmail.com>
2013-05-09 20:28:04 +0200

    Memory extra supported: lb, lh, lbu, sh, sb

--- src/components/MemoryUnit.java
+++ src/components/MemoryUnit.java
@@ -2,45 +2,97 @@ package components;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import exceptions.MemoryOutOfBoundsException;
 
 public class MemoryUnit {
-	
+
 	byte[] memory;
-	
-	public MemoryUnit(){
-		memory = new byte[1<<20];
+
+	public MemoryUnit() {
+		memory = new byte[1 << 10];
 	}
-	
-	public MemoryUnit(int size){
+
+	public MemoryUnit(int size) {
 		memory = new byte[size];
 	}
-	
-	public static byte[] break_integer(int value){
-	    return new byte[] {
-	            (byte)(value >>> 24),
-	            (byte)(value >>> 16),
-	            (byte)(value >>> 8),
-	            (byte)value};
+
+	public static byte[] break_word(int value) {
+		return new byte[] { (byte) (value >>> 24), (byte) (value >>> 16),
+				(byte) (value >>> 8), (byte) value };
 	}
 	
-	
-	public static int connect_integer(byte[] array){
+	public static byte[] break_half_word (int value){
+		return new byte[] { (byte) (value >>> 8), (byte) value};
+	}
+
+	public static int connect_word(byte[] array) {
 		return ByteBuffer.wrap(array).getInt();
 	}
+
+	public static int connect_half_word(byte[] array) {
+		return ByteBuffer.wrap(array).getShort();
+	}
 	
-	public void store_word(int index, int value){
+	public static int connect_byte(byte[] array) {
+		return ByteBuffer.wrap(array).get();
+	}
+
+	public byte[] read(int start, int end) {
+		if (start < 0 || end > memory.length) {
+			throw new MemoryOutOfBoundsException();
+		}
 		
-		byte[] array = break_integer(value);
+		return Arrays.copyOfRange(memory, start, end);
+
+	}
+	
+	public void write(int index, byte[]array){
 		
-		for(int i=index; i<index+4; i++){
+		if(index < 0 || index+array.length > memory.length)
+			throw new MemoryOutOfBoundsException();
+				
+		for(int i=index; i-index < array.length; i++){
 			memory[i] = array[i-index];
 		}
 		
 	}
 	
-	public int load_word(int index){
-		byte[] array = Arrays.copyOfRange(memory, index, index+4);
-		return connect_integer(array);
+	public int size(){
+		return memory.length;
+	}
+
+	public int load_word(int index) {
+		byte[] array = read(index, index + 4);
+		return connect_word(array);
+	}
+
+	public int load_half_word(int index) {
+		byte[] array = read(index, index + 2);
+		return connect_half_word(array);
+	}
+
+	public int load_byte(int index) {
+		byte[] array = read(index, index+1);
+		return connect_byte(array);
+	}
+	
+	public int load_byte_unsigned(int index){
+		byte[] array = read(index, index+1);
+		return (short) array[0] & 0xff;
+	}
+
+	public void store_word(int index, int value) {
+		byte[] array = break_word(value);
+		write(index,array);
+	}
+	
+	public void store_half_word(int index, int value){
+		byte[] array = break_half_word(value);
+		write(index,array);
+	}
+
+	public void store_byte(int index, int value) {
+		write(index, new byte[] {((byte) (value & 0x000000FF))});
 	}
 
 }
